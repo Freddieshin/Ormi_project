33ea7c6dbe9b7fc3edc2bdfbef55af348b4bc148
Freddieshin <168792932+Freddieshin@users.noreply.github.com>
2024-05-28 00:18:10 +0900

    0528 최종ver

--- 프로젝트.js
+++ 프로젝트.js
@@ -19,7 +19,20 @@ async function fetchImages(page){
     console.error('데이터를 가져오는데 문제가 발생했습니다 :', error);
   }
 }
-
+function makeImageList(datas){
+  datas.forEach((data)=>{
+    listPic.insertAdjacentHTML('beforeend', `<li class="moreimg"><img src="${data.download_url}" alt="추가고양이"></li>`);
+  });
+}
+<!-- 카카오맵 api구현 -->
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=8ccd9c3f2c7564d10cde92b8dbd1884a"></script>
+  var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+  mapOption = {
+  center: new kakao.maps.LatLng(37.4745063, 126.8897151), // 지도의 중심좌표
+  level: 3 // 지도의 확대 레벨
+};
+  // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+  var map = new kakao.maps.Map(mapContainer, mapOption);
 <!-- 모달구현 -->
 document.querySelector("#openmodal").addEventListener("click", function (){document.querySelector(".modal").style.display = 'flex'})
 document.querySelector("#close").addEventListener("click", function (){document.querySelector(".modal").style.display = 'none'})
